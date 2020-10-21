20cbd7816bc51639c7f576036b7fc95f19815ce0
seriousdave42 <seriousdave42@gmail.com>
2020-10-20 19:09:58 -0500

    added repositories

---
+++ src/main/java/com/dwatkins/relationships/repositories/PersonRepository.java
@@ -0,0 +1,13 @@
+package com.dwatkins.relationships.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dwatkins.relationships.models.Person;
+
+@Repository
+public interface PersonRepository extends CrudRepository<Person, Long> {
+	List<Person> findAll();
+}
